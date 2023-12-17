818986126e87583fe02ba35ed792158ec4d0e880
kravchenkoAnanas <anastasiaa.kravchenkoo@gmail.com>
2023-12-17 14:53:53 +0100

    add fibonacci & sortings pages

--- src/components/fibonacci-page/fibonacci-page.tsx
+++ src/components/fibonacci-page/fibonacci-page.tsx
@@ -1,10 +1,92 @@
-import React from "react";
+import React, { ChangeEvent, FormEvent, useEffect, useState } from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import { Input } from "../ui/input/input";
+import { Button } from "../ui/button/button";
+import { Circle } from "../ui/circle/circle";
+import { ElementStates } from "../../types/element-states";
 
 export const FibonacciPage: React.FC = () => {
+  const [i, setI] = useState(0);
+  const [n, setN] = useState(0);
+  const [input, setInput] = useState('');
+  const [isLoader, setIsLoder] = useState(false);
+  const [arrayToAnimate, setArrayToAnimate] = useState<any[]>([]);
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (!isLoader) {
+      setInput(e.target.value);
+      console.log(e.target.value);
+    }
+  };
+
+  const submit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("CLICK input=", input);
+    if (!isLoader) {
+      setIsLoder(true);
+
+      setArrayToAnimate([1]);
+      setN(Number(input));
+      setI(0);
+
+      console.log("CLICK input=", input, " Number(input) ", Number(input));
+    }
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (isLoader) {
+        if (i < n) {
+          const array: any[] = arrayToAnimate.slice();
+          console.log('animation', array, " i ", i);
+          let val = 1;
+          if (i !== 0) {
+            console.log("array.push(array[i] + array[i - 1]);", array[i], array[i - 1])
+            val = array[i] + array[i - 1];
+          }
+          setI(i + 1);
+          array.push(val);
+          console.log(array);
+          setArrayToAnimate(array);
+        } else {
+          setIsLoder(false);
+        }
+      }
+    }, 500);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [isLoader, i, arrayToAnimate]);
+
   return (
     <SolutionLayout title="Последовательность Фибоначчи">
-     
+      <form action="" onSubmit={ submit }>
+        <div style={{ maxWidth: '50%', margin: 'auto', display: 'flex', gap: '2.5%' }}>
+          <Input max={19} isLimitText={true} type={"number"} onChange={onChange}></Input>
+          <Button
+            type="submit"
+            text={"Рассчитать"}
+            // isLoader={isLoader}
+          >
+          </Button>
+        </div>
+        <div
+          className=""
+          style={{ maxWidth: '50%', paddingTop: '5%', margin: 'auto', display: 'flex', justifyContent: 'center', gap: '2.5%' }}
+        >
+          {arrayToAnimate && arrayToAnimate.map((item, index) => {
+            return (
+                <Circle
+                  index={ index }
+                  letter={ item }
+                  // state={ StaticRange. }
+                >
+                </Circle>
+          )})
+          }
+        </div>
+      </form>
     </SolutionLayout>
   );
 };
--- src/components/sorting-page/sorting-page.tsx
+++ src/components/sorting-page/sorting-page.tsx
@@ -1,10 +1,259 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import { RadioInput } from "../ui/radio-input/radio-input";
+import { Button } from "../ui/button/button";
+import { Direction } from "../../types/direction";
+import { Column } from "../ui/column/column";
+import { ElementStates } from "../../types/element-states";
+
+const randomArr = (minLen: number = 3, maxLen: number = 17) => {
+  const length = Math.max(minLen, Math.floor(Math.random() * maxLen));
+  const array =  Array.from({length: length}, () => Math.floor(Math.random() * 100));
+  return array.map((item) => {
+    return {
+      data: item,
+      state: ElementStates.Default
+    };
+  });
+}
+
+const swap = (arr: any[], i: number, j: number) => {
+  const temp = arr[i]['data'];
+  arr[i]['data'] = arr[j]['data'];
+  arr[j]['data'] = temp;
+};
 
 export const SortingPage: React.FC = () => {
+  const [isLoader, setIsLoder] = useState<boolean>(false);
+  const [arrayToAnimate, setArrayToAnimate] = useState<any[]>(randomArr());
+  const [sortType, setSortType] = useState<string>(''); // asc / desc
+  const [sortAlgoType, setSortAlgoType] = useState<string>('selection'); // selection / bubble
+  const [sortIndex, setSortIndex] = useState<number>(0);
+  const [i, setI] = useState<number>(0);
+  const [j, setJ] = useState<number>(0);
+
+  const cmp = (first: number, second: number) => {
+    if (sortType === "asc") {
+      return first < second;
+    } else if (sortType === "desc") {
+      return first > second;
+    } else {
+      console.log("cmp error sortType", sortType);
+    }
+  };
+
+  const onClick = (type: any) => {
+    if (["selection", "bubble"].includes(type)) {
+      setSortAlgoType(type);
+    } else if (["asc", "desc"].includes(type)) {
+      if (sortAlgoType === 'selection') {
+        setI(0);
+        setJ(1);
+      } else if (sortAlgoType === "bubble") {
+        setI(0);
+        setJ(0);
+      }
+      setArrayToAnimate(
+        arrayToAnimate.map((item) => {
+          return {
+            data: item['data'],
+            state: ElementStates.Default
+          };
+        })
+      );
+      setSortType(type);
+      setIsLoder(true);
+    } else if (type === "newArray") {
+      setIsLoder(false);
+      setArrayToAnimate(randomArr());
+    } else {
+      console.log("onClick error type", type)
+    }
+  };
+
+  const submit = (e: any) => {
+    e.preventDefault();
+
+    let sortAlgo: any = null;
+    const radios: any = document.getElementsByName("sortType");
+    for(let i = 0; i < radios.length; i++) {
+      if (radios[i].checked) {
+        sortAlgo = radios[i].value;
+      }
+    }
+    setSortAlgoType(sortAlgo);
+    console.log("type ", sortType, " algoType ", sortAlgoType);
+  };
+
+  const bubbleSortAnimationStep = (array: any[]) => {
+    console.log("Bubble i ", i, " j ", j);
+  
+    if (j > 0) {
+      array[j - 1]['state'] = ElementStates.Default;
+      array[j]['state'] = ElementStates.Default;
+    }
+
+    if (cmp(array[j + 1]['data'], array[j]['data'])) {
+      swap(array, j, j + 1);
+    }
+    array[j]['state'] = ElementStates.Changing;
+    array[j + 1]['state'] = ElementStates.Changing;
+  
+    if (i + 1 === array.length) {
+      array[j]['state'] = ElementStates.Modified;
+      array[j + 1]['state'] = ElementStates.Modified;
+      setIsLoder(false);
+      setI(0);
+      setJ(0);
+    }
+    else if (j + 1 === array.length - i - 1) {
+      array[j]['state'] = ElementStates.Default;
+      array[j + 1]['state'] = ElementStates.Modified;
+      setJ(0);
+      setI(i + 1);
+    } else {
+      setJ(j + 1);
+    }
+  };
+
+  const selectionSortAnimationStep = (array: any[]) => {
+    console.log("Selection i ", i, " j ", j, " sortIndex ", sortIndex);
+
+    array[j - 1]['state'] = ElementStates.Default;
+    array[i]['state'] = ElementStates.Changing;
+
+    if (j < array.length) {
+      array[j]['state'] = ElementStates.Changing;
+      
+      if (j === i + 1 || cmp(array[j]['data'], array[sortIndex]['data'])) {
+        setSortIndex(j);
+      }
+    }
+
+    if (i + 1 === array.length) {
+      array[i]['state'] = ElementStates.Modified;
+
+      setIsLoder(false);
+      setI(0);
+      setJ(1);
+    }
+    else if (j > array.length - 1) {
+      array[i]['state'] = ElementStates.Modified;
+      if (cmp(array[sortIndex]['data'], array[i]['data'])) {
+        swap(array, sortIndex, i);
+      }
+
+      setSortIndex(i + 1);
+      setJ(i + 2);
+      setI(i + 1);
+    } else {
+      setJ(j + 1);
+    }
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (isLoader) {
+        if (true) {
+          const array: any[] = arrayToAnimate.slice();
+          if (sortAlgoType === "bubble") {
+            bubbleSortAnimationStep(array);
+          } else if (sortAlgoType === "selection") {
+            selectionSortAnimationStep(array);
+          }
+          setArrayToAnimate(array);
+        }
+      } else {
+        setIsLoder(false);
+        if (sortAlgoType == "selection") {
+          setI(0);
+          setJ(1);
+        } else if (sortAlgoType == "bubble") {
+          setI(0);
+          setJ(0);
+        }
+      }
+    }, 200);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [isLoader, arrayToAnimate, i, j]);
+  
   return (
     <SolutionLayout title="Сортировка массива">
-
+      <form action="" onSubmit={ submit }>
+        <div
+          className=""
+          style={{ maxWidth: '70%', margin: 'auto', display: 'flex', justifyContent: 'center', gap: '5%' }}
+        >
+          <div
+            className=""
+            style={{ alignItems: 'center', display: 'flex', gap: '10%' }}
+          >
+            <RadioInput
+              label={"Выбор"}
+              name={"sortType"}
+              value={"selection"}
+              checked={ sortAlgoType === "selection" }
+              onChange={() => { onClick("selection") }}
+            ></RadioInput>
+            <RadioInput
+              label={"Пузырёк"}
+              name={"sortType"}
+              value={"bubble"}
+              checked={ sortAlgoType === "bubble" }
+              onChange={() => { onClick("bubble") }}
+            ></RadioInput>
+          </div>
+          <div
+            className=""
+            style={{ minWidth: '40%', display: 'flex', gap: '5%' }}
+          >
+            <div onClick={() => { onClick("asc") }}>
+              <Button
+                  id={"asc"}
+                  type="submit"
+                  text={"По возрастанию"}
+                  sorting={Direction.Ascending}
+                  name={"asc"}
+                  value={"asc"}
+                  >
+                </Button>
+            </div>
+            <div onClick={() => { onClick("desc") }}>
+              <Button
+                id={"desc"}
+                type="submit"
+                text={"По убыванию"}
+                sorting={Direction.Descending}
+                name={"desc"}
+                value={"desc"}
+                >
+              </Button>
+            </div>
+          </div>
+          <Button
+            type="submit"
+            text={"Новый массив"}
+            value={"newArray"}
+            onClick={ () => { onClick("newArray") } }
+            >
+          </Button>
+        </div>
+      </form>
+      <div
+          className=""
+          style={{ minHeight: '300px', maxWidth: '50%', paddingTop: '5%', margin: 'auto', display: 'flex', alignItems: 'end', justifyContent: 'center', gap: '2.5%' }}
+      >
+        {arrayToAnimate && arrayToAnimate.map((item, index) => {
+            return <Column
+              key={index} 
+              index={item['data']}
+              state={item['state']}
+            ></Column>
+        })}
+      </div>
     </SolutionLayout>
   );
 };
