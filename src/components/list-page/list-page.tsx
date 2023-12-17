95d5069192658c8b84f12626e773bad608e48117
kravchenkoAnanas <anastasiaa.kravchenkoo@gmail.com>
2023-12-17 21:50:27 +0100

    add queue & list

--- src/components/list-page/list-page.tsx
+++ src/components/list-page/list-page.tsx
@@ -1,10 +1,218 @@
-import React from "react";
+import React, { ChangeEvent, useEffect, useState } from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import { Input } from "../ui/input/input";
+import { Button } from "../ui/button/button";
+import { Circle } from "../ui/circle/circle";
+import { ElementStates } from "../../types/element-states";
+import { ArrowIcon } from "../ui/icons/arrow-icon";
+import { stat } from "fs";
+
 
 export const ListPage: React.FC = () => {
+  const initialArray = [0, 34, 8, 1];
+  const [isLoader, setIsLoder] = useState(false);
+  const [action, setAction] = useState<string | null>(''); // push / pop
+  const [input, setInput] = useState('');
+  const [direction, setDirection] = useState(''); // front / back
+  const [arrayToAnimate, setArrayToAnimate] = useState<any[]>(initialArray.map(item => {
+    return {
+      data: item,
+      smallUpperData: null,
+      smallLowerData: null,
+      state: ElementStates.Default,
+    }
+  }));
+  const [headIdx, setHeadIdx] = useState<number | null>(0);
+  const [tailIdx, setTailIdx] = useState<number | null>(initialArray.length - 1);
+
+  const onChangeInput = (e: ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value);
+  };
+
+  const onClkickPush = (type: string) => {
+    if (!input || !Number(input)) {
+      return;
+    }
+
+    let inputSelector = document.getElementById('inputValue') as HTMLInputElement;
+    inputSelector.value = '';
+
+    const array: any[] = arrayToAnimate.slice();
+    const idx = type === "front" ? 0 : array.length - 1;
+    setDirection(type);
+    array[idx]['smallUpperData'] = input;
+    setArrayToAnimate(array);
+    setIsLoder(true);
+    setAction("push");
+    setInput("");
+  };
+  
+  const onClkickPop = (type: string) => {
+    if (!arrayToAnimate.length) {
+      return;
+    }
+
+    const array: any[] = arrayToAnimate.slice();
+    const idx = type === "front" ? 0 : array.length - 1;
+    setDirection(type);
+    array[idx]['smallLowerData'] = array[idx]['data'];
+    array[idx]['data'] = null;
+    setArrayToAnimate(array);
+
+    setIsLoder(true);
+    setAction("pop");
+  };
+
+  const onClkickDrop = (e: any) => {
+    setArrayToAnimate(Array(7).fill({
+      data: null,
+      state: ElementStates.Default,
+    }));
+    setHeadIdx(null);
+    setTailIdx(null); 
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (isLoader) {
+        if (action === "push") {
+          const idx = direction === 'front' ? 0 : arrayToAnimate.length - 1;
+          const value = arrayToAnimate[idx]['smallUpperData'];
+          const newItem = {data: value, state: ElementStates.Modified, smallUpperData: null, smallLowerData: null};
+          const array: any[] = arrayToAnimate.slice();
+          array[idx]['smallUpperData'] = null;
+          setAction("push2");
+
+          if (direction === 'front') {
+            setArrayToAnimate([newItem, ...array]);
+          } else {
+            setArrayToAnimate([...array, newItem]);
+          }
+        } else if (action === "push2") {
+          const array: any[] = arrayToAnimate.slice();
+          array[direction == 'front' ? 0 : arrayToAnimate.length - 1]['state'] = ElementStates.Default;
+          setArrayToAnimate(array);
+          setAction(null);
+        } else if (action === "pop") {
+          setAction(null);
+          if (direction === 'front') {
+            setArrayToAnimate(arrayToAnimate.slice(1));
+          } else {
+            setArrayToAnimate(arrayToAnimate.slice(0, arrayToAnimate.length - 1));
+          }
+        }
+        if (action === null) {
+          setIsLoder(false);
+        }
+      }
+    }, 500);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [isLoader, arrayToAnimate]);
+
   return (
     <SolutionLayout title="Связный список">
+      <div style={{ display: 'flex', flexDirection: 'column', gap: '30%' }}>
+        <div
+          style={{ minWidth: '60%', margin: 'auto', display: 'flex', justifyContent: 'space-between', gap: '1%' }}
+        >
+          <div>
+            <Input
+              id={"inputValue"}
+              maxLength={4}
+              isLimitText={true}
+              placeholder={"Введите значение"}
+              onChange={onChangeInput}
+            ></Input>
+          </div>
+          <Button
+            type="button"
+            text={"Добавить в head"}
+            onClick={() => onClkickPush("front") }
+            isLoader={isLoader && action === "push" && direction === "front"}
+            disabled={isLoader && action !== "push" && direction === "front" || !input}
+            ></Button>
+          <Button
+            type="button"
+            text={"Добавить в tail"}
+            onClick={() => onClkickPush("back") }
+            isLoader={isLoader && action === "push" && direction === "back"}
+            disabled={isLoader && action !== "push" && direction === "back" || !input}
+          ></Button>
+          <Button
+            type="button"
+            text={"Удалить из head"}
+            onClick={() => onClkickPop("front") }
+            isLoader={isLoader && action === "pop" && direction === "front"}
+            disabled={isLoader && action !== "pop" && direction === "front" || !arrayToAnimate.length}
+          ></Button>
+          <Button
+            type="button"
+            text={"Удалить из tail"}
+            onClick={() => onClkickPop("back") }
+            isLoader={isLoader && action === "pop" && direction === "back"}
+            disabled={isLoader && action !== "pop" && direction === "back" || !arrayToAnimate.length}
+          ></Button>
+        </div>
+        <div
+          style={{ minWidth: '60%', margin: 'auto', display: 'flex', justifyContent: 'space-between'}}
+        >
+          <div>
+            <Input
+              id={"inputIdx"}
+              placeholder={"Введите индекс"}
+              onChange={onChangeInput}
+              style={{ minWidth: '20%'}}
+            ></Input>
+          </div>
 
+          <Button
+            type="button"
+            text={"Добавить в head"}
+            // onClick={onClkickPush}
+            // isLoader={isLoader && action === "push"}
+            // disabled={isLoader && action !== "push" || !input}
+            disabled={true}
+            style={{ minWidth: '35%'}}
+          ></Button>
+          <Button
+            type="button"
+            text={"Удалить из head"}
+            // onClick={onClkickPop}
+            // isLoader={isLoader && action === "pop"}
+            // disabled={isLoader && action !== "pop" || headIdx === null}
+            disabled={true}
+            style={{ minWidth: '35%'}}
+          ></Button>
+        </div>
+      </div>
+      <div
+          style={{ height: '300px', maxWidth: '100%', paddingTop: '2%', margin: 'auto', display: 'flex', alignItems: 'center', justifyContent: 'center', gap: '2.5%' }}
+      >
+        {arrayToAnimate && arrayToAnimate.map((item, index) => {
+          return <>
+            <Circle
+              key={index} 
+              index={index}
+              letter={item['data']}
+              state={item['state']}
+              head={item['smallUpperData'] !== null
+                ?  <Circle key={index} letter={item['smallUpperData']} state={ElementStates.Changing} isSmall={true}></Circle> 
+                : index === 0 ? 'head' : null
+              }
+              tail={item['smallLowerData'] !== null
+                ? <Circle key={index+1} letter={item['smallLowerData']} state={ElementStates.Changing} isSmall={true}></Circle>
+                : index === arrayToAnimate.length - 1 ? 'tail' : null
+              }
+            ></Circle>
+            {index !== arrayToAnimate.length - 1 &&
+              <ArrowIcon></ArrowIcon>
+            }
+          </>
+        })}
+      </div>
     </SolutionLayout>
   );
 };
--- src/components/queue-page/queue-page.tsx
+++ src/components/queue-page/queue-page.tsx
@@ -1,10 +1,163 @@
-import React from "react";
+import React, { ChangeEvent, useEffect, useState } from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import { Input } from "../ui/input/input";
+import { Button } from "../ui/button/button";
+import { Circle } from "../ui/circle/circle";
+import { ElementStates } from "../../types/element-states";
+
 
 export const QueuePage: React.FC = () => {
+  const [isLoader, setIsLoder] = useState(false);
+  const [action, setAction] = useState(""); // push / pop
+  const [input, setInput] = useState('');
+  const [arrayToAnimate, setArrayToAnimate] = useState<any[]>(Array(7).fill({
+      data: null,
+      state: ElementStates.Default,
+  }));
+  const [headIdx, setHeadIdx] = useState<number | null>(null);
+  const [tailIdx, setTailIdx] = useState<number | null>(null);
+
+  const onChangeInput = (e: ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value);
+  };
+
+  const onClkickPush = (e: any) => {
+    if (!input || !Number(input) || tailIdx === arrayToAnimate.length - 1) {
+      return;
+    }
+
+    let inputSelector = document.getElementById('input') as HTMLInputElement;
+    inputSelector.value = '';
+
+    let newTailIdx = null;
+
+    if (tailIdx === null) {
+      newTailIdx = 0;
+      setHeadIdx(0);
+    } else if (tailIdx !== arrayToAnimate.length - 1) {
+      newTailIdx = tailIdx + 1
+    }
+
+    const array: any[] = arrayToAnimate.slice();
+    array[newTailIdx ?? 0] = {data: input, state: ElementStates.Changing};
+    setTailIdx(newTailIdx);
+    setArrayToAnimate(array);
+    setIsLoder(true);
+    setAction("push");
+    setInput("");
+  };
+  
+  const onClkickPop = (e: any) => {
+    if (headIdx === null) {
+      return;
+    }
+
+    const array: any[] = arrayToAnimate.slice();
+    array[headIdx ?? 0]['state'] = ElementStates.Changing;
+    setArrayToAnimate(array);
+
+    setIsLoder(true);
+    setAction("pop");
+  };
+
+  const onClkickDrop = (e: any) => {
+    setArrayToAnimate(Array(7).fill({
+      data: null,
+      state: ElementStates.Default,
+    }));
+    setHeadIdx(null);
+    setTailIdx(null); 
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (isLoader) {
+        if (action === "push") {
+          arrayToAnimate[tailIdx ?? 0]['state'] = ElementStates.Default;
+        } else if (action === "pop") {
+          const array: any[] = arrayToAnimate.slice();
+          array[headIdx ?? 0]['data'] = null;
+          array[headIdx ?? 0]['state'] = ElementStates.Default;
+
+          let newHeadIdx = null;
+          if (headIdx === null) {
+            newHeadIdx = 0;
+            setTailIdx(0);
+          } else if (headIdx !== arrayToAnimate.length - 1) {
+            if (headIdx === tailIdx) {
+              setTailIdx(null);
+              newHeadIdx = headIdx
+            } else {
+              newHeadIdx = headIdx + 1;
+            }
+          }
+          
+          setArrayToAnimate(array);
+          setHeadIdx(newHeadIdx);
+        } 
+        setIsLoder(false);
+      }
+    }, 500);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [isLoader, arrayToAnimate]);
+
   return (
     <SolutionLayout title="Очередь">
+      <div
+        className=""
+        style={{ maxWidth: '70%', margin: 'auto', display: 'flex', justifyContent: 'center', gap: '5%' }}
+      >
+        <Input
+          id={"input"}
+          maxLength={4}
+          isLimitText={true}
+          placeholder={"Введите значение"}
+          onChange={onChangeInput}
+        ></Input>
 
+        <div style={{ display: 'flex', gap: '5%' }}>
+          <Button
+            type="button"
+            text={"Добавить"}
+            onClick={onClkickPush}
+            isLoader={isLoader && action === "push"}
+            disabled={isLoader && action !== "push" || !input}
+          ></Button>
+          <Button
+            type="button"
+            text={"Удалить"}
+            onClick={onClkickPop}
+            isLoader={isLoader && action === "pop"}
+            disabled={isLoader && action !== "pop" || headIdx === null}
+          ></Button>
+        </div>
+        <div className="" style={{ display: 'flex', gap: '5%' }}>
+          <Button
+            type="button"
+            text={"Очистить"}
+            onClick={onClkickDrop}
+            disabled={isLoader || headIdx === null}
+          ></Button>
+        </div>
+      </div>
+      <div
+          className=""
+          style={{ maxWidth: '50%', paddingTop: '10%', margin: 'auto', display: 'flex', alignItems: 'end', justifyContent: 'center', gap: '2.5%' }}
+      >
+        {arrayToAnimate && arrayToAnimate.map((item, index) => {
+          return <Circle
+            key={index} 
+            index={index}
+            letter={item['data']}
+            state={item['state']}
+            head={index === headIdx ? "head" : ""}
+            tail={index === tailIdx ? "tail" : ""}
+          ></Circle>
+        })}
+      </div>
     </SolutionLayout>
   );
 };
