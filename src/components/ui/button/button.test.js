3e84dc51157066833197efaec120f6f2df8e7045
kprmk <kpriemko@gmail.com>
2024-01-20 10:50:29 +0100

    upd tests with describe & install and setup cypress

--- src/components/ui/button/button.test.js
+++ src/components/ui/button/button.test.js
@@ -2,51 +2,53 @@ import renderer from 'react-test-renderer';
 import { Button } from './button';
 import { render, screen, fireEvent, getByTestId } from '@testing-library/react';
 
-const TEST_SNAPSHOT_BUTTON_WITH_TEXT = 'Рендер кнопки с текстом';
-const TEST_SNAPSHOT_BUTTON_WITHOUT_TEXT = 'Рендер кнопки без текстом';
-const TEST_SNAPSHOT_BUTTON_DISABLED = 'Рендер заблокированной кнопки';
-const TEST_SNAPSHOT_BUTTON_WITH_LOADER = 'Рендер кнопки с индикацией загрузки';
+const TEST_SNAPSHOT_BUTTON_WITH_TEXT = 'с текстом';
+const TEST_SNAPSHOT_BUTTON_WITHOUT_TEXT = 'без текстом';
+const TEST_SNAPSHOT_BUTTON_DISABLED = 'блокированная';
+const TEST_SNAPSHOT_BUTTON_WITH_LOADER = 'с индикацией загрузки';
 
 const TEST_BUTTON_CLICK = 'Вызов колбека при клике на кнопку';
 
-it(TEST_SNAPSHOT_BUTTON_WITH_TEXT, () => {
-  const tree = renderer
-    .create(<Button text={"Развернуть"} />)
-    .toJSON();
-    expect(tree).toMatchSnapshot(TEST_SNAPSHOT_BUTTON_WITH_TEXT);
-});
+describe("Кнопка", () => {
+  it(TEST_SNAPSHOT_BUTTON_WITH_TEXT, () => {
+    const tree = renderer
+      .create(<Button text={"Развернуть"} />)
+      .toJSON();
+      expect(tree).toMatchSnapshot(TEST_SNAPSHOT_BUTTON_WITH_TEXT);
+  });
 
-it(TEST_SNAPSHOT_BUTTON_WITHOUT_TEXT, () => {
-  const tree = renderer
-    .create(<Button text={""} />)
-    .toJSON();
-    expect(tree).toMatchSnapshot(TEST_SNAPSHOT_BUTTON_WITHOUT_TEXT);
-});
+  it(TEST_SNAPSHOT_BUTTON_WITHOUT_TEXT, () => {
+    const tree = renderer
+      .create(<Button text={""} />)
+      .toJSON();
+      expect(tree).toMatchSnapshot(TEST_SNAPSHOT_BUTTON_WITHOUT_TEXT);
+  });
 
-it(TEST_SNAPSHOT_BUTTON_DISABLED, () => {
-  const tree = renderer
-    .create(<Button disabled={true} />)
-    .toJSON();
-    expect(tree).toMatchSnapshot(TEST_SNAPSHOT_BUTTON_DISABLED);
-});
+  it(TEST_SNAPSHOT_BUTTON_DISABLED, () => {
+    const tree = renderer
+      .create(<Button disabled={true} />)
+      .toJSON();
+      expect(tree).toMatchSnapshot(TEST_SNAPSHOT_BUTTON_DISABLED);
+  });
 
-it(TEST_SNAPSHOT_BUTTON_WITH_LOADER, () => {
-  const tree = renderer
-    .create(<Button isLoader={true} />)
-    .toJSON();
-    expect(tree).toMatchSnapshot(TEST_SNAPSHOT_BUTTON_WITH_LOADER);
-});
+  it(TEST_SNAPSHOT_BUTTON_WITH_LOADER, () => {
+    const tree = renderer
+      .create(<Button isLoader={true} />)
+      .toJSON();
+      expect(tree).toMatchSnapshot(TEST_SNAPSHOT_BUTTON_WITH_LOADER);
+  });
 
-it(TEST_BUTTON_CLICK, () => {
-    const callback = jest.fn();
+  it(TEST_BUTTON_CLICK, () => {
+      const callback = jest.fn();
 
-    const { container } = render(<Button
-        data-testid="button"
-        onClick={ callback() }
-    />);
+      const { container } = render(<Button
+          data-testid="button"
+          onClick={ callback() }
+      />);
 
-    const button = getByTestId(container, "button");
+      const button = getByTestId(container, "button");
 
-    fireEvent.click(button);
-    expect(callback).toHaveBeenCalledTimes(1);
+      fireEvent.click(button);
+      expect(callback).toHaveBeenCalledTimes(1);
+  });
 });
