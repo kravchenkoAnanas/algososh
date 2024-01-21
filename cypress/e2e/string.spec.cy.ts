a9ea2b10251930a2245f085f5dd6d1d17d6fcc4c
kprmk <kpriemko@gmail.com>
2024-01-21 09:33:35 +0100

    string e2e test

--- cypress/e2e/string.spec.cy.ts
+++ cypress/e2e/string.spec.cy.ts
@@ -13,19 +13,51 @@ describe('app works correctly with routes', function() {
     cy.get('[data-testid="button"]').should('be.disabled');
   });
   
-  it('check', function() {
+  it('check animation', function() {
     const testString = 'test';
     const resultStirngs = ['test', 'tset'];
-    const expectedLength = resultStirng.length;
+    const expectedLength = testString.length;
 
     cy.get('[data-testid="input"]').type(testString);
     cy.get('[data-testid="button"]').click();
 
+    cy.get('[data-testid="result"]').children().should('have.length', expectedLength);
     cy.get('[data-testid="result"]').children().each(($el, index) => {
-      // cy.wrap($el).should('have.class', 'expected-class-for-step-' + index);
-      cy.wrap($el).invoke('text').should('eq', resultStirng[index]);
+      cy.wrap($el).invoke('text').should('eq', resultStirngs[0][index]);
+
+      const toMatch = index === 0 || index === expectedLength - 1 ? /changing/ : /default/;
+      cy.wrap($el).find('div[class*="circle_circle"]').invoke('attr', 'class').then((className) => {
+        expect(className).to.match(toMatch);
+      });
+    });
+    cy.wait(1000);
+
+    cy.get('[data-testid="result"]').children().each(($el, index) => {
+      cy.wrap($el).invoke('text').should('eq', resultStirngs[0][index]);
+
+      const toMatch = index === 1 || index === expectedLength - 2 ? /changing/ : /modified/;
+      cy.wrap($el).find('div[class*="circle_circle"]').invoke('attr', 'class').then((className) => {
+        expect(className).to.match(toMatch);
+      });
+    });
+    cy.wait(1000);
+
+    cy.get('[data-testid="result"]').children().each(($el, index) => {
+      cy.wrap($el).invoke('text').should('eq', resultStirngs[1][index]);
+
+      const toMatch = index === 1 || index === expectedLength - 2 ? /changing/ : /modified/;
+      cy.wrap($el).find('div[class*="circle_circle"]').invoke('attr', 'class').then((className) => {
+        expect(className).to.match(toMatch);
+      });
     });
 
-    cy.get('[data-testid="result"]').children().should('have.length', expectedLength);
+    cy.wait(1000);
+    cy.get('[data-testid="result"]').children().each(($el, index) => {
+      cy.wrap($el).invoke('text').should('eq', resultStirngs[1][index]);
+
+      cy.wrap($el).find('div[class*="circle_circle"]').invoke('attr', 'class').then((className) => {
+        expect(className).to.match(/modified/);
+      });
+    });
   });
 }); 
